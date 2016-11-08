839848e8e4e3a12a1612f95fb20d4def4392eb52
Oliver Lai <oliverlai55@gmail.com>
2016-11-08 11:40:56 -0500

    Set up function to close picture

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 
+
 function expand() {
+
   console.log('fire off');
-  console.log(document.getElementById('pic1').style);
+
+    document.getElementById('pic1').setAttribute('id', 'overlay-pic');
+    document.getElementById('close-icon').style.display = "inline";
+
+
+
   // document.getElementById('id').innerHTML = 'yo';
 }
 
+function closePic() {
+  document.getElementById('overlay-pic').setAttribute('id', 'pic1');
+}
 // 1. Add attribute to selected picture
 // 2. make that picture expand in width with black div blackground
