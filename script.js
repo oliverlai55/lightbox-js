84799ba5799d01b998bb4079b6523d01737336eb
oliverlai55 <oliverlai55@gmail.com>
2016-11-07 22:47:20 -0500

    Add comment for future steps

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ function expand() {
   console.log(document.getElementById('pic1').style);
   // document.getElementById('id').innerHTML = 'yo';
 }
+
+// 1. Add attribute to selected picture
+// 2. make that picture expand in width with black div blackground
