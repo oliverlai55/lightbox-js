297829e5e59e751018d78d6e52b4313e2829f321
Oliver Lai <oliverlai55@gmail.com>
2016-11-10 15:32:20 -0500

    Change eventListner to add class

--- script.js
+++ script.js
@@ -12,13 +12,11 @@ function expand() {
     document.getElementById('pic1').setAttribute('id', 'overlay-pic');
     document.getElementById('close-icon').style.display = "inline";
 
-
-
   // document.getElementById('id').innerHTML = 'yo';
 }
 
 function closePic() {
-  document.getElementById('overlay-pic').setAttribute('id', 'pic1');
+  document.getElementsByClassName('expand')[0].classList.remove('expand');
   document.getElementById('close-icon').style.display = "none";
 }
 // 1. Add attribute to selected picture
@@ -27,13 +25,18 @@ function closePic() {
 // document.getElementById('pic1').addEventListener('click', function() {
 //   console.log(document.getElementsByClassName('picture'));
 
+
 var pictureClass = document.getElementsByClassName('picture')
 
 for (var i = 0; i < pictureClass.length; i++) {
   // var position = pictureClass[i]
   pictureClass[i].addEventListener('click', function(position) {
-    console.log(position.target.id);
+    //change position to event
+    debugger;
+    // console.log(position.target.id);
     var pictureId = position.target.id;
-    document.getElementById(pictureId).setAttribute('id', 'overlay-pic');
+    document.getElementById(pictureId).classList.add('expand');
+    console.log(document.getElementById(pictureId).classList);
+    document.getElementById('close-icon').style.display = "inline";
   });
 }
